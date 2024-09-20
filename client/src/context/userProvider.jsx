d348e758547b9c824b6aceaac65dd37973405cd8
DiegoMHB <dmhb17@gmail.com>
2024-09-20 19:51:54 +0200

    userContext working

--- client/src/context/userProvider.jsx
+++ client/src/context/userProvider.jsx
@@ -1,39 +1,37 @@
-// import { useEffect, useState } from "react";
-// import { UserContext } from "./userContext";
-// import data from "../mockData/mock-users.json";
+import { useEffect, useState } from "react";
+import { UserContext } from "./userContext";
+import data from "../mockData/mock-users.json";
 
-// export const UserProvider = ({ children }) => {
-//   const [user, setUser] = useState({});
-//   // const [userId,setUserId] = useState('');
+export const UserProvider = ({ children }) => {
 
-//   const userId = ("3"); //ill need to get the id of the user
+  const [user, setUser] = useState({});
+  const [id, setId] = useState('');
 
-
-//   useEffect(() => {
-//     for (let user of data.users) {
-
-//       if (user.userId === userId) {
-//         const el = {
-//           userId: user.userId,
-//           username: user.username,
-//           posts: user.posts,
-//           details: user.details,
-//           profile_picture: user.profile_picture,
-//           dateLogin: user.dateLogin
-//         };
-//         setUser(el);
-//       }
-//     }
-//   }, []);
+  useEffect(() => {
+    for (let user of data.users) {
+      if (user.userId === id) {
+        const el = {
+          userId: user.userId,
+          username: user.username,
+          posts: user.posts,
+          details: user.details,
+          profile_picture: user.profile_picture,
+          dateLogin: user.dateLogin
+        };
+        setUser(el);
+      }
+    }
+  }, [id])
 
 
-//   return (
-//     <UserContext.Provider
-//       value={{
-//         user,
-//       }}
-//     >
-//       {children}
-//     </UserContext.Provider>
-//   );
-// };
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setId
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import { UserProvider } from "./context/userProvider.jsx";
+import { UserProvider } from "./context/userProvider.jsx";
 import {PostProvider} from "./context/postProvider.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <UserProvider> */}
+    <UserProvider>
       <PostProvider>
         <App />
       </PostProvider>
-    {/* </UserProvider> */}
+    </UserProvider>
   </StrictMode>
 );
