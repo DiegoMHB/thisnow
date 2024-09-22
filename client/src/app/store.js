71ecaeec8cedabc95a280d5cd1585792fc042a6e
DiegoMHB <dmhb17@gmail.com>
2024-09-22 11:34:25 +0200

    idem

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/userSLice'
+import userReducer from '../features/userSLice';
+import mapReducer from '../features/mapSlice';
+import postsReducer from '../features/postsSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    map: mapReducer,
+    posts: postsReducer,
   },
 })
